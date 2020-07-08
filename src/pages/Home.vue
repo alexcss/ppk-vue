12d184927c71560f01977cd336ba0ac6ac8f7f1c
AlexCSS <kalyuzhnyi@gmail.com>
2020-07-08 12:49:23 +0300

    deploy config

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,9 @@
 <template>
-  <main>
-    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+   <main>
+      <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
 
-    <Intro v-bind:section="sections.intro" />
-  </main>
+      <Intro v-bind:section="sections.intro" />
+   </main>
 </template>
 
 <script>
@@ -11,25 +11,25 @@
 import Intro from "@/components/home/Intro.vue";
 
 export default {
-  name: "Home",
-  data() {
-    return {
-      title: "Home page",
-      sections: {},
-    };
-  },
-  components: {
-    Intro,
-  },
+   name: "Home",
+   data() {
+      return {
+         title: "Home page",
+         sections: {},
+      };
+   },
+   components: {
+      Intro,
+   },
 
-  beforeCreate() {
-    fetch("data/home-page.json")
-      .then((result) => result.json())
-      .then((data) => {
-        console.log(data);
+   beforeCreate() {
+      fetch("data/home-page.json")
+         .then((result) => result.json())
+         .then((data) => {
+            console.log(data);
 
-        this.sections = data;
-      });
-  },
+            this.sections = data;
+         });
+   },
 };
 </script>
